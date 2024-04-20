53f8fc5845a3eaae70299fd7eca6f576169cd439
Eslam Alaa <eslamalaa532@gmail.com>
2024-04-20 18:53:46 +0200

    last

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-// import StarRating from "./Components/StarRating";
 import "./index.css";
 import App from "./App";
-
-// function Test() {
-//   const [movieRating, setmovieRating] = useState(0);
-//   return (
-//     <div>
-//       <StarRating
-//         color="blue"
-//         starColor="purple"
-//         maxRating={10}
-//         onSetRating={setmovieRating}
-//       />
-//       <p>this movie was rated {movieRating} stars</p>
-//     </div>
-//   );
-// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -25,16 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-//    <StarRating
-// maxRating={5}
-// messages={["one", "two", "three", "four", "good"]}
-// />
-// <StarRating
-// maxRating={10}
-// size={40}
-// color="black"
-// borderColor="green"
-// starColor="red"
-// defaultRating={4}
-// />
-// <Test />
